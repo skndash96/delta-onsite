e7757f37fdb109670ee951e9c8cddec78b7671e0
skndash96 <dashskndash@gmail.com>
2025-07-22 16:09:30 +0000

    done

---
+++ README.md
@@ -0,0 +1,4 @@
+# Delta ONSITE Task
+The script analyses the folder at /home/skndash96/httpd (Apache server code with recovered modified date) and produces the report as per the given instructions and gives output at `/reports/[epoch seconds]`  
+Sample of the report is available in the repo root.
+For the cronjob part, it should work if this script is moved to cron.daily folder at `/etc/cron.daily`
--- onsite.sh
+++ onsite.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [[ "$(whoami)" != "root" ]]; then
+  echo "This script must be run as root"
+  exit 1
+fi
+
 OUT_DIR="/reports"
 mkdir -p "$OUT_DIR"
 
@@ -8,11 +13,13 @@ EXT_FILE=$(mktemp)
 YMS_FILE=$(mktemp)
 SIZ_FILE=$(mktemp)
 PMS_FILE=$(mktemp)
+TMP_FILES=$(mktemp)
 
 touch "$OUT_FILE"
 
-find httpd > files.txt
-readarray -n 1000 files < files.txt
+# below path must be absolute
+find /home/skndash96/httpd > $TMP_FILES
+readarray -n 1000 files < $TMP_FILES
 
 declare -A extensions
 declare -A yms
@@ -22,7 +29,7 @@ declare -A pms
 for file in "${files[@]}"; do
   file=$(echo "$file" | tr -d '\n')
 
-  if [[ ! -f "$PWD/$file" ]]; then
+  if [[ ! -f "$file" ]]; then
     echo "Skipping $file, not a file"
     continue
   fi
@@ -34,18 +41,18 @@ for file in "${files[@]}"; do
     ext=${name##*.}
   fi
 
-  # mod=`stat -c %Y "$PWD/$file"`  # doesnt work why?
-  size=`stat -c %s "$PWD/$file"`
-  mod=`stat -c %y "$PWD/$file"`
-  urwx=`stat -c %A "$PWD/$file"`
-  octal=`stat -c %a "$PWD/$file"`
+  # mod=`stat -c %Y "$file"`  # doesnt work why?
+  size=`stat -c %s "$file"`
+  mod=`stat -c %y "$file"`
+  urwx=`stat -c %A "$file"`
+  octal=`stat -c %a "$file"`
   ym=${mod:0:7}
 
   echo "$size $file" >> $SIZ_FILE
 
   yms[$ym]=$(( ${yms[$ym]:-0} + 1 ))
   if [[ yms[$ym] -le "10" ]]; then
-    yms_array[$ym]=`echo "${yms_array[$ym]:-}$file"`
+    yms_array[$ym]=`echo "${yms_array[$ym]:-}###$file"`
   fi
 
   extensions[$ext]=$(( ${extensions[$ext]:-0} + 1 ))
@@ -57,8 +64,7 @@ for ext in "${!extensions[@]}"; do
 done
 
 for ym in "${!yms[@]}"; do
-  echo "$ym: ${yms[$ym]}" >> $YMS_FILE
-  echo "${yms_array[$ym]}" >> $YMS_FILE
+  echo "$ym: ${yms[$ym]}${yms_array[$ym]}" >> $YMS_FILE
   echo "" >> $YMS_FILE
 done
 
@@ -70,7 +76,7 @@ echo "Report at $OUT_FILE" > $OUT_FILE
 echo "Total files: ${#files[@]}" >> $OUT_FILE
 
 echo -e "\nMost Active Months" >> $OUT_FILE
-cat $YMS_FILE | sort -k2 -n -r | head -n 10 >> $OUT_FILE
+cat $YMS_FILE | sort -k2 -n -r | head -n 10 | sed 's/###/\n/g' >> $OUT_FILE
 
 echo -e "\nMost Common Extensions" >> $OUT_FILE
 cat $EXT_FILE | sort -k2 -n -r | head -n 10 >> $OUT_FILE
@@ -88,3 +94,5 @@ rm -f "$EXT_FILE" "$YMS_FILE" "$SIZ_FILE" "$PMS_FILE"
 
 echo "Older reports cleaning.."
 find $OUT_DIR -type f -mtime +7 -delete
+
+# For the cronjob part, move this script to cron.daily and it should work.
